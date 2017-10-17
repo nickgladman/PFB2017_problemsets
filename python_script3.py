ac7cc248834e86b29f7e85476ad25fa19030d3a1
nickgladman <gladman@cshl.edu>
2017-10-17 16:28:05 -0400

    final python_script3.py added, contains bonus features

---
+++ python_script3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#import modules we have had examples on so far
+import sys
+import math
+
+#define variable from command line input
+x = float(sys.argv[1])
+
+if x > 0:
+	print(x, "is a positive number.")
+	if x == 50:
+		print(x, "is a nice number, but this script can do cool stuff with other values; try another.")
+	if x > 50:
+		print(x, "is greater than 50.")
+		if x%3 == 0:
+			print("Also", x, "is divisible by 3.")
+	elif x < 50:
+		print(x, "is less than 50.")
+		if x%2 == 0:
+			print("Also", x, "is an even number.")
+		else:
+			print("Also", x, "is an odd number.")
+else:
+	print(x, "is a negative number, and we don't like to deal in negatives: this is a happy place.")
+
