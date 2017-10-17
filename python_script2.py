d4c03933464ef8aad4e933916344c8e3174529d7
nickgladman <gladman@cshl.edu>
2017-10-17 16:02:29 -0400

    added python_scirpt2.py

---
+++ python_script2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#import modules we have had examples on so far
+import sys
+import math
+
+#define variable
+x = 10
+
+#if_else statment that prints out confirmation of truth if True or 'Not True' if False
+if x > 7:
+	print("10 is greater than 7")
+else:
+	print('Not True')
