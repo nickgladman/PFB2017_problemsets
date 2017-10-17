5f45ccdcc710bfcb6a53944d96d149baee8629ae
nickgladman <gladman@cshl.edu>
2017-10-17 12:02:54 -0400

    updated what the variables mean in the script for stdin promptin

--- python_script1.py
+++ python_script1.py
@@ -2,10 +2,10 @@
 import sys
 
 #setting command line inputs to sys.argv list variables
-a = sys.argv[1]
-b = sys.argv[2]
-c = sys.argv[3]
-d = sys.argv[4] 
+a = sys.argv[1] #name
+b = sys.argv[2] #favorite color
+c = sys.argv[3]	#favorite thing to do
+d = sys.argv[4] #favorite animal
 
 #print list variables defined from sys.argv[] inputs
 print(a)
