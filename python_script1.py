ca64a658baa06715530af4f174e54997bb608ee5
nickgladman <gladman@cshl.edu>
2017-10-17 11:25:54 -0400

    adding first python script python_scirpt.py

---
+++ python_script1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+#setting command line inputs to sys.argv list variables
+a = sys.argv[1]
+b = sys.argv[2]
+c = sys.argv[3]
+d = sys.argv[4] 
+
+#print list variables defined from sys.argv[] inputs
+print(a)
+print(b)
+print(c)
+print(d)
