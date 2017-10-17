7c074f38ecbd1add0356dc29700b496773ba208f
nickgladman <gladman@cshl.edu>
2017-10-17 16:09:19 -0400

    modified python_script2.py to take args from stdin

--- python_script2.py
+++ python_script2.py
@@ -4,11 +4,11 @@
 import sys
 import math
 
-#define variable
-x = 10
+#define variable from command line input
+x = float(sys.argv[1])
 
 #if_else statment that prints out confirmation of truth if True or 'Not True' if False
 if x > 7:
-	print("10 is greater than 7")
+	print(x, "is greater than 7")
 else:
-	print('Not True')
+	print('Not True. ', x, " is not greater than 7")
