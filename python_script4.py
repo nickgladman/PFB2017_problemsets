47cd64e67ddaabec36d46f3480f93a5a93ac3f44
nickgladman <gladman@cshl.edu>
2017-10-18 13:46:49 -0400

    corrrectly followed instructions. Added fragment analysis portion of python_script4.py

--- python_script4.py
+++ python_script4.py
@@ -29,12 +29,22 @@ dna_reverse_complement = dna_complement[::-1]
 
 ecor1 = "GAATTC"
 ecor1_start_site = 1 + int(dna.find("GAATTC"))
+ecor1_cut_site = 2 + int(dna.find("GAATCC"))
 ecor1_end_site = ecor1_start_site + 5
 
+#formating output of above scripts to print out a string with given indexes
+#and is tab separated
 ecor1_print = '''The recognition sequence, start, and end site for EcoR1 are: 
 {0}\t{1}\t{2}.'''
 
 print(ecor1_print.format(ecor1, ecor1_start_site, ecor1_end_site))
+
+
+#add the restriction fragments to a list
+
+ecor1_fragments_list = [ecor1,str(ecor1_start_site), str(ecor1_end_site)]
+
+
 
 
 
