6704163c5d473b5768503773bf2b0b17beb8f52a
nickgladman <gladman@cshl.edu>
2017-10-18 15:55:40 -0400

    print 100 and factorial of 1000 while loops added

---
+++ python_script5.py
@@ -0,0 +1,18 @@
+!# usr/bin/env python3
+
+
+#while loop to print out numbers 1 to 100
+
+count = 1
+while count <101:
+	print(count)
+	count += 1
+
+#while loop to calculate factorial of 1000
+
+count = 1
+factorial = 1000
+while count < 1000:
+	factorial = factorial * count
+	count += 1
+print(factorial)
