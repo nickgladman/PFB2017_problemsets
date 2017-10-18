fb81844098e7df6c327d4d52dae8cd131164b2d5
nickgladman <gladman@cshl.edu>
2017-10-18 16:15:39 -0400

    odd and even sum element added

--- python_script5.py
+++ python_script5.py
@@ -16,3 +16,29 @@ while count < 1000:
 	factorial = factorial * count
 	count += 1
 print(factorial)
+
+
+#for loop iteration. Find and print even numbers.
+
+numbers =  [101,2,15,22,95,33,2,27,72,15,52]
+
+for num in numbers:
+	if num%2  == 0:
+		print(num)
+
+#sort list using .sort
+
+numbers.sort()
+
+even_sum = 0
+odd_sum = 0
+for num in numbers:
+	print(num)
+	if num % 2 == 0:
+		even_sum += num
+	if num % 2 == 1:
+		odd_sum += num
+	
+print(even_sum)
+print(odd_sum)
+
